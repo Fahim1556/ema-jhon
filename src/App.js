2a3ed9724b49105dfa6e72dd3a800f94cc3b14b6
Fahim1556 <49098556+Fahim1556@users.noreply.github.com>
2020-09-13 00:08:28 +0600

    adding after auth

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
@@ -13,14 +13,22 @@ import Review from './components/Review/Review';
 import Inventory from './components/Inventory/Inventory';
 import NotFound from './components/NotFound/NotFound';
 import ProductDetail from './components/ProductDetail/ProductDetail';
+import Shipment from './components/Shipment/Shipment';
+import Login from './components/Login/Login';
+import { createContext } from 'react';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+
+export const UserContext = createContext();
 
 
 
-function App() {
+function App(props) {
+  const [loggedInUser, setLoggedInUser] = useState({});
   return (
-    <div>
-    <Header></Header>
+  <UserContext.Provider value = {[loggedInUser,setLoggedInUser]}>
+    
        <Router>
+         <Header></Header>
         <Switch>
            <Route path="/shop">
             <Shop></Shop>
@@ -28,8 +36,14 @@ function App() {
            <Route path="/review">
             <Review></Review>
            </Route>
-           <Route path="/inventory">
+           <PrivateRoute path="/inventory">
               <Inventory></Inventory>
+           </PrivateRoute>
+           <PrivateRoute path="/shipment">
+              <Shipment></Shipment>
+           </PrivateRoute>
+           <Route path="/login">
+              <Login></Login>
            </Route>
            <Route exact path="/">
             <Shop></Shop>
@@ -45,7 +59,7 @@ function App() {
         <Shop></Shop>
         
        </Router>
-    </div>
+    </UserContext.Provider>
     
   );
 }
---
+++ src/components/Login/Login.js
@@ -0,0 +1,137 @@
+import React, { useState } from 'react';
+import { useContext } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+import { UserContext } from '../../App';
+import { createUserWithEmailAndPassword, handleFBLogin, handleGoogleSignIn, handleSignOut, initializeLoginFramework, signInWithEmailAndPassword } from './loginManager';
+
+//  firebase.initializeApp(firebaseConfig);
+
+function Login() {
+  const [newUser,setNewUser] = useState(false);
+  const [user,setUser] = useState({
+     inSignedIn: false,
+     
+     name:'',
+     email:'',
+     password:'',
+     photo:''
+    
+  });
+
+  initializeLoginFramework();
+
+  const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+  const history = useHistory();
+  const location = useLocation();
+  let {from} = location.state || {from: {pathname: "/"} };
+  
+  const googleSignIn = () => {
+    handleGoogleSignIn ()
+    .then(res => {
+      handleResponse(res,true);
+    })
+  }
+
+  const fbLogin = () => {
+    handleFBLogin()
+    .then(res => {
+      handleResponse(res,true);
+    })
+
+  }
+
+  const signOut = () => {
+    handleSignOut()
+    .then(res => {
+      handleResponse(res,false);
+    })
+  }
+
+  const handleResponse = (res , redirect) => {
+    setUser(res);
+    setLoggedInUser(res);
+    if(redirect){
+      history.replace(from);
+    }
+  }
+
+  const handleBlur = (e) => {
+     
+    let isFormValid =true;
+    if(e.target.name === 'email'){
+      isFormValid = /\S+@\S+\.\S+/.test(e.target.value);
+    }
+
+    if(e.target.name === 'password'){ 
+      const isPasswordValid = e.target.value.length > 6;
+      const passwordHasNumber = /\d{1}/.test(e.target.value);
+      isFormValid = isPasswordValid && passwordHasNumber;
+    }
+    if(isFormValid){ 
+      const newUserInfo = {...user};
+      newUserInfo[e.target.name] = e.target.value;
+      setUser(newUserInfo);
+    }
+  
+  }
+
+  const handleSubmit =(e) => {
+     if(newUser && user.email && user.password){
+       createUserWithEmailAndPassword(user.name, user.email, user.password)
+       .then(res => {
+        handleResponse(res,true);
+       })
+     }
+
+     if(!newUser && user.email && user.password){
+        signInWithEmailAndPassword(user.email, user.password)
+        .then(res => {
+          handleResponse(res,true);
+         })
+     }
+     e.preventDefault();
+  }
+
+  
+
+  return (
+    <div style={{textAlign:'center'}}>
+       {
+         user.isSignedIn ? <button onClick={signOut}>Sign out</button> :
+         <button onClick={googleSignIn}>Sign in</button>
+       }
+
+       <br/>
+       <button onClick={fbLogin}>Sign in using Facebook</button>
+
+      {
+        user.isSignedIn &&
+         <div>
+          <p> Welcome,{user.name}</p>
+          <p>your mail:{user.email}</p>
+          <img src={user.photo} alt=""></img>
+         </div>
+      }
+ 
+
+      <h1>Our own Authentication</h1> 
+      <input type="checkbox" onChange={() => setNewUser(!newUser) } name="newUser" id=""/>
+      <label htmlFor="newUser">New user Sign up</label>
+       
+      <form onSubmit={handleSubmit}>
+        { user.newUser && <input name="name" type="text" onBlur={handleBlur} placeholder="Your name"/> }
+        <br/>
+        <input type="text" name="email" onBlur={handleBlur} placeholder="enter your email"/>
+        <br/>
+        <input type="password" name="password" onBlur={handleBlur} placeholder="your password"/>
+        <br/>
+        <input type="submit" value="Submit"/>
+      </form>
+      <p style={{color:'red'}}>{user.error}</p>
+      { user.success &&  <p style={{color:'green'}}>User Created succesfully</p>}
+      
+    </div>
+  );
+}
+
+export default Login;
